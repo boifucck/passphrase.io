48164247f05178fa2e635add2c4c30dfa3275014
Daniel Routman <routman@protonmail.ch>
2016-08-13 14:46:04 -0600

    text formatting, random passphrase

--- js/script.js
+++ js/script.js
@@ -10,89 +10,122 @@
 	var salt = "passphrase.io";
 	var hash;
 
-	function pad(s, size) {
-		while(s.length < size) s = "0" + s;
-		return s;
-	}
+	$(document).ready(function(){
 
-	function getText() {
-		$('#button').html('loading');
-		$('#button').css('color','#AAAAAA');
-		var passphrase = $('#passphrase').val();
-		passphrase = passphrase.toString();
+		function pad(s, size) {
+			while(s.length < size) s = "0" + s;
+			return s;
+		}
 
-		scrypt(passphrase, salt, logN, r, L, step,
-      		function(progress) {
-                $('#progressbar').width(progress +'%');
-            },
-            function(result) {
-            	$('#progressbar').width('100%');
-	   			hash = CryptoJS.SHA256(result);
-				hash = hash.toString();
-				hash = pad(hash, 64);
+		function getText() {
+			$('#button').html('loading');
+			$('#button').css('color','#AAAAAA');
+			var passphrase = $('#passphrase').val();
+			passphrase = passphrase.toString();
+
+			scrypt(passphrase, salt, logN, r, L, step,
+	      		function(progress) {
+	                $('#progressbar').width(progress +'%');
+	            },
+	            function(result) {
+	            	$('#progressbar').width('0');
+		   			hash = CryptoJS.SHA256(result);
+					hash = hash.toString();
+					hash = pad(hash, 64);
+					$.ajax({
+						url: 'gettext.php',
+						type: 'post',
+						data: {'hash': hash},
+						success: function(data) {
+							if (data == "") {
+								quill.setHTML('');
+							}
+							else {
+								var decrypted = CryptoJS.AES.decrypt(data,passphrase).toString(CryptoJS.enc.Utf8);
+								quill.setHTML(decrypted.replace(/\n/g, "<br />"));
+							}
+							$('#button').html('loaded');
+							$('#notepad').css('color','#000000');
+							quill.editor.enable();
+						},
+						error: function () {       
+							$('#button').html('error');
+						}
+					});
+				},
+	        "hex");
+		}
+
+		function saveText() {
+			var passphrase = $('#passphrase').val();
+			passphrase = passphrase.toString();
+
+			if (passphrase != "") {
+				var rawtext = quill.getHTML();
+
+				rawtext = rawtext.substring(0,32768);
+
+				var encrypted = CryptoJS.AES.encrypt(rawtext, passphrase);
+				encrypted = encrypted.toString();
+
+				$('#button').html('saving');
 				$.ajax({
-					url: 'gettext.php',
+					url: 'savetext.php',
 					type: 'post',
-					data: {'hash': hash},
+					data: {'hash': hash, 'text': encrypted},
 					success: function(data) {
-						if (data == "") {
-							$('#notepad').val('');
-						}
-						else {
-							var decrypted = CryptoJS.AES.decrypt(data,passphrase).toString(CryptoJS.enc.Utf8);
-							$('#notepad').val(decrypted);
-						}
-						$('#button').html('loaded');
-						$('#notepad').css('color','#000000');
-						$("#notepad").prop("readonly", false);
+						console.log('save successful');
+						$('#button').html('saved');
+
 					},
 					error: function () {       
 						$('#button').html('error');
 					}
-				});
+				});	
+			} 
+		}
+
+		var quill = new Quill('#notepad', {
+			modules: {
+			    'link-tooltip': true,
+			    'image-tooltip': true,
 			},
-        "hex");
-	}
+			styles: {
+				'.ql-container': {
+					'font-size': "16px"
+			    }
+			},
+			theme: 'snow'
+		});
 
-	function saveText() {
-		var passphrase = $('#passphrase').val();
-		passphrase = passphrase.toString();
+  		quill.addModule('toolbar', { container: '#toolbar' });
 
-		if (passphrase != "") {
-			var rawtext = $('#notepad').val();
-			rawtext = rawtext.toString();
-			rawtext = rawtext.substring(0,10000);
-			var encrypted = CryptoJS.AES.encrypt(rawtext, passphrase);
-			encrypted = encrypted.toString();
-			$('#button').html('saving');
-			$.ajax({
-				url: 'savetext.php',
-				type: 'post',
-				data: {'hash': hash, 'text': encrypted},
-				success: function(data) {
-					console.log('save successful');
-					$('#button').html('saved');
+  		quill.editor.disable();
 
-				},
-				error: function () {       
-					$('#button').html('error');
-				}
-			});	
-		} 
-	}
-
-	$(document).ready(function(){
-
-		$('#hide').click(function(){
+		$('.hide').click(function(){
 			if ($('#passphrase').attr('type') == 'text') {
-				$('#hide').html('[show passphrase]');
+				$('#hide-normal').html('show passphrase');
+				$('#hide-mobile').html('show');
 				$('#passphrase').attr('type', 'password');
 			}
 			else {
-				$('#hide').html('[hide passphrase]');
+				$('#hide-normal').html('hide passphrase');
+				$('#hide-mobile').html('hide');
 				$('#passphrase').attr('type', 'text');
 			}
 
+		});
+
+		$('#random').click(function(){
+			var pk = secureRandom(32);
+	        var seed = Crypto.util.bytesToHex(pk.slice(0,16));
+	        //nb! electrum doesn't handle trailing zeros very well
+	        if (seed.charAt(0) == '0') seed = seed.substr(1);
+	        $('#passphrase').val(mn_encode(seed));
+	        $('#button').html('load');
+	        $('#button').css('color','#000000');
+	        $('#notepad').css('color','#AAAAAA');
+	        quill.setHTML('click load');
 		});
 
 		$('#passphrase').keypress(function(e) {
@@ -102,7 +135,7 @@
 				var label = $('#button').html();
 				if (passphrase != "" && label != "loading") {
 		        	$('#passphrase').blur();
-		        	$('#notepad').val('loading...');
+		        	quill.setHTML('loading...');
 		        	getText();
 		        }
 		    }
@@ -121,12 +154,28 @@
 			else {
 				$('#button').css('color','#000000');
 			}
-			$('#notepad').val('hit enter to load');
+			quill.setHTML('hit enter to load');
 			$('#notepad').css('color','#AAAAAA');
-			$("#notepad").prop("readonly", true);
+			quill.editor.disable();
 		});
 
 		$('#notepad').keyup(function(){
+			var label = $('#button').html();
+			if (label == 'loaded' || label == 'saved' || label == 'error') {
+				$('#button').html('save');
+				$('#button').css('color','#000000');
+			}
+		});
+
+		$('.ql-size, .ql-format-button').click(function(){
+			var label = $('#button').html();
+			if (label == 'loaded' || label == 'saved' || label == 'error') {
+				$('#button').html('save');
+				$('#button').css('color','#000000');
+			}
+		});
+
+		$('.ql-font, .ql-size, .ql-color, .ql-background, .ql-align').change(function(){
 			var label = $('#button').html();
 			if (label == 'loaded' || label == 'saved' || label == 'error') {
 				$('#button').html('save');
